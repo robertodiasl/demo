82de3ca4ebc911903a1130d1cc5f67523ef9a87f
Roberto Dias <robertoboajunior@gmail.com>
2025-08-19 22:18:34 -0300

    inicio

---
+++ src/main/java/com/example/demo/entity/Aluno.java
@@ -0,0 +1,69 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+@Entity
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Date dataNasc;
+    private String email;
+    private String senha;
+
+    public Aluno(String nome, Date dataNasc, String email, String senha) {
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Aluno() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
